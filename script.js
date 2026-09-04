bdfb44c79c41858bc54229c314c4c85aa6ebcc8a
neoroskin-source <neoroskin@gmail.com>
2026-09-04 12:13:46 +0800

    script.js

--- script.js
+++ script.js
@@ -265,6 +265,6 @@ async function loadServicesFromSupabase() {
 // Load public Supabase data
 if (!document.querySelector('script[src*="public-data.js"]')) {
   const publicDataScript = document.createElement('script');
-  publicDataScript.src = 'public-data.js?v=508';
+  publicDataScript.src = 'public-data.js?v=509';
   document.body.appendChild(publicDataScript);
 }
